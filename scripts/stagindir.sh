492179940c7f0484e2ddb96e0f9906089c311fa2
Miguel Angel Falcón <falconmfm3@yahoo.es>
2017-06-07 03:30:31 +0200

    Se consigue crear una vm con rhks sin salto de lineas.

--- scripts/stagindir.sh
+++ scripts/stagindir.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -eux
 
 # Install EPEL repository.
-mkdir /tmp/stagin
-mkdir ansible
+#echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
+#echo "Defaults:vagrant !requiretty" >> /etc/sudoers.d/vagrant
+#chmod 0440 /etc/sudoers.d/vagrant
+#mkdir -pm 700 "/home/vagrant/.ssh/"
+curl -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+#cat <<EOF >/home/vagrant/.ssh/authorized_keys
+#ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
+#EOF
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant "/home/vagrant/.ssh"
 # Install Ansible.
-chmod 755 /tmp/stagin ansible 
+ 
